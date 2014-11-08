1e4802ea43e84dba2d9acefae8530e02973b185b
tieluxiangmu <herbertmeng@foxmail.com>
2014-11-08 13:10:01 +0800

    *联系人控制器修改

--- web/protected/controllers/ContactController.php
+++ web/protected/controllers/ContactController.php
@@ -6,9 +6,13 @@ class ContactController extends Controller
 	{
 		$contactmodel=new Contact();
         //查询联系人 需要根据字段修改
-        $contactinfo=$contactmodel->findBySql("SELECT * FROM `tl_contact` ",array());
+//        $contactinfo=$contactmodel->findBySql("SELECT * FROM `tl_contact` ",array());
         $smarty = Yii::app()->smarty;
-        $smarty->_smarty->assign('data',$contactinfo);
+//        $smarty->_smarty->assign('data',$contactinfo);
+        $smarty->_smarty->assign('data',array(
+                                                'username'=>'张三',
+                                                'tel'=>'12312312',
+                                                'other'=>'asdasd');
         $smarty->_smarty->display('contact/page/view.tpl');
     }
 
